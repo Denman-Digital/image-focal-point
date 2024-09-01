3f59808b7c67c139e9577f5f0e48ab4b426afd2e
Paul Walton <paul@denmandigital.com>
2024-08-31 21:14:20 -0700

    v1.1

--- image-bg-postion.php
+++ image-bg-postion.php
@@ -1,56 +1,76 @@
 <?php
 
 /**
- * Plugin Name: Image Background Focus Position
+ * Plugin Name: WP Image Focal Point
  * Description: Set background focus position for the media images
- * Version: 1.0
- * Author: florianmatthias
- * Author URI: https://florianmatthias.com/
+ * Version: 1.1
+ * Author: Denman Digital
+ * Author URI: https://denman.digital/
  * Tested up to: 5.7
  * License: GPLv3 or later
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
- *
+ * text-domain: wp-img-focal-point
  */
 
-//custom media field
-function custom_media_add_media_custom_field($form_fields, $post)
+namespace WP_Image_Focal_Point;
+
+use WP_Post;
+
+function add_attachment_custom_field($form_fields, $post)
 {
-	$field_value = get_post_meta($post->ID, 'bg_pos_desktop', true);
-	$disabled = ($field_value && $field_value != '50% 50%') ? '' : 'style="display:none"';
-	$label = ($field_value && $field_value != '50% 50%') ? 'Change' : 'Set';
-	$desktop_text = ($field_value && $field_value != '50% 50%') ? '<b>Desktop</b>: ' . $field_value : '<b>Desktop</b>: Centered (default)';
-	$field_value = ($field_value) ? $field_value : '50% 50%';
+	$field_value = get_post_meta($post->ID, 'focal_point', true) ?: '50% 50%';
+	$is_value_not_default = $field_value != '50% 50%';
+
+	$label_instructions = esc_html__("Click on the image to set the focus point", "wp-img-focal-point");
+	$label_cancel = esc_html__("Cancel", "wp-img-focal-point");
+	$label_save = esc_html__("Save", "wp-img-focal-point");
+	$label_change = esc_html__("Change", "wp-img-focal-point");
+	$label_set = esc_html__("Set", "wp-img-focal-point");
+	$label_reset = esc_html__("Reset", "wp-img-focal-point");
+	$label_default = esc_html__('Centered (default)', "wp-img-focal-point");
 
 	$html = "
-			<input type='hidden' value='" . $field_value . "'	id='bg_pos_desktop_id' name='attachments[$post->ID][bg_pos_desktop]'>
-			<input type='hidden' value='" . $field_value_2 . "'	id='bg_pos_mobile_id' name='attachments[$post->ID][bg_pos_mobile]'>
-			<div class='overlay image_focus_point'>
+			<input type='hidden' value='$field_value'	id='focal_point_hidden_input' name='attachments[$post->ID][focal_point]'>
+			<div id='focal-point-overlay' class='overlay'>
 				<div class='img-container'>
 					<div class='header'>
 						<div class='wrapp'>
-							<h3>Click on the image to set the focus point</h3>
+							<h3>$label_instructions</h3>
 							<div class='controls'>
-								<span class='button button-secondary' onclick='cancel_focus()'>Cancel</span>
-								<span class='button button-primary' onclick='close_overlay()'>Save</span>
+								<span class='button button-secondary' onclick='WP_Image_Focal_Point.cancelFocus()'>$label_cancel</span>
+								<span class='button button-primary' onclick='WP_Image_Focal_Point.closeOverlay()'>$label_save</span>
 							</div>
 						</div>
 					</div>
 					<div class='container'>
-						<div class='pin'></div>
-						<img src='" . wp_get_attachment_url($post->ID) . "'>
+						<div class='pin-field'>
+							<div class='pin'></div>
+							<img id='focal-point-image' draggable='false' src='" . wp_get_attachment_url($post->ID) . "'>
+						</div>
 					</div>
 				</div>
 			</div>
-			<div class='focusp_label_holder'>
-				<div id='desktop_value'>" . $desktop_text . "</div>
-				<input type='button' class='button button-small' value='" . $label . "' id='label_desktop' onclick='set_focus(0)'>
-				<span class='close button button-small' id='reset_desktop' " . $disabled . " onclick='reset_focus()'>Reset</span>
+			<div id='focal-point-dashboard'>
+				<div id='focal-point-value'>" . ($is_value_not_default ? esc_html($field_value) : $label_default) . "</div>
+				<input
+					type='button'
+					id='focal-point-set'
+					class='button button-small'
+					onclick='WP_Image_Focal_Point.setFocus()'
+					value='" . ($is_value_not_default ? $label_change : $label_set) . "'>
+				<input
+					type='button'
+					id='focal-point-reset'
+					class='close button button-small " . ($is_value_not_default ? '' : 'button-disabled') ."'
+					onclick='WP_Image_Focal_Point.resetFocus()'
+					value='$label_reset'" .
+					($is_value_not_default ? '' : 'aria-disabled="true"') . ">
 			</div>
 		";
 
 	$form_fields['background_postion_desktop'] = array(
-		'value' => $field_value ? $field_value : '',
-		'label' => __('Focus Point'),
+		'value' => $field_value ?: '',
+		'label' => __('Focal Point', "wp-img-focal-point"),
 		'helps' => __(''),
 		'input'  => 'html',
 		'html' => $html
@@ -58,36 +78,50 @@ function custom_media_add_media_custom_field($form_fields, $post)
 
 	return $form_fields;
 }
-add_filter('attachment_fields_to_edit', 'custom_media_add_media_custom_field', null, 2);
+add_filter('attachment_fields_to_edit', __NAMESPACE__ . '\add_attachment_custom_field', null, 2);
 
 //save custom media field
-function custom_media_save_attachment($attachment_id)
+function save_attachment_custom_field($attachment_id)
 {
-	if (isset($_REQUEST['attachments'][$attachment_id]['bg_pos_desktop'])) {
-		$bg_pos_desktop = $_REQUEST['attachments'][$attachment_id]['bg_pos_desktop'];
-		update_post_meta($attachment_id, 'bg_pos_desktop', $bg_pos_desktop);
+	if (isset($_REQUEST['attachments'][$attachment_id]['focal_point'])) {
+		$focal_point = $_REQUEST['attachments'][$attachment_id]['focal_point'];
+		update_post_meta($attachment_id, 'focal_point', $focal_point);
 	}
 }
-add_action('edit_attachment', 'custom_media_save_attachment');
+add_action('edit_attachment', __NAMESPACE__ . '\save_attachment_custom_field');
 
-//apply filter in frontend to object-position
-function filter_gallery_img_atts(array $attrs, WP_Post $attachment)
+/**
+ * Add object position to images on frontend (defaults to centered if not set)
+ * @param array $attrs
+ * @param WP_Post $attachment
+ * @return array
+ */
+function filter_gallery_img_atts(array $attrs, WP_Post $attachment): array
 {
 	$styles = $attrs["style"] ?? "";
 	if ($styles) {
 		$styles .= ";";
 	}
-	$focal_point = get_post_meta($attachment->ID, "bg_pos_desktop", true);
+	$focal_point = get_post_meta($attachment->ID, "focal_point", true);
 	$attrs["style"] = $styles . "object-position:" . ((string) $focal_point ?: "50% 50%") . ";";
 	return $attrs;
 }
-add_filter('wp_get_attachment_image_attributes', 'filter_gallery_img_atts', 10, 2);
+add_filter('wp_get_attachment_image_attributes', __NAMESPACE__ . '\filter_gallery_img_atts', 10, 2);
 
-//enqueue script in admin
-function image_bg_admin_scripts()
+/**
+ * Enqueue script in Admin
+ */
+function enqueue_admin_scripts()
 {
-	wp_enqueue_style('image-bg-css', plugin_dir_url(__FILE__) . '/admin.css?_=1' . time());
-	wp_enqueue_script('image-bg-js', plugin_dir_url(__FILE__) . '/script.js?_=1' . time());
+	wp_enqueue_style('wp-image-focal-point-css', plugin_dir_url(__FILE__) . '/admin.css?_=1' . time());
+	wp_enqueue_script('wp-image-focal-point-js', plugin_dir_url(__FILE__) . '/script.js?_=1' . time(), ["jquery"]);
+	wp_localize_script("wp-image-focal-point-js", __NAMESPACE__, [
+		"labels" => [
+			"change" => esc_html__("Change", "wp-img-focal-point"),
+			"set" => esc_html__("Set", "wp-img-focal-point"),
+			"reset" => esc_html__("Reset", "wp-img-focal-point"),
+			"default" => esc_html__('Centered (default)', "wp-img-focal-point"),
+		]
+	]);
 }
-
-add_action('admin_enqueue_scripts', 'image_bg_admin_scripts');
+add_action('admin_enqueue_scripts', __NAMESPACE__ . '\enqueue_admin_scripts');
--- script.js
+++ script.js
@@ -1,123 +1,69 @@
-let $ = jQuery;
-let d_left = null;
-let d_right = null;
-let m_left = null;
-let m_right = null;
-let mobile_position = false;
-let bgX = null;
-let bgY = null;
-let centered_text = 'Centered (default)';
-let mobile_use_desktop_text = 'Desktop focus point (default)';
-let imageWt = 0;
-let imageHt = 0;
-let containerHt = 0;
-let containerWt = 0;
+(function ($, WP_Image_Focal_Point) {
+	let focalX = null;
+	let focalY = null;
 
-function set_bg_values(){
-	let d = $("#bg_pos_desktop_id").val().replaceAll('%','');
-	let m = $("#bg_pos_mobile_id").val().replaceAll('%','');
-	d_left = d.split(' ')[0];
-	d_right = d.split(' ')[1];
-	m_left = m.split(' ')[0];
-	m_right = m.split(' ')[1];
-}
-function cancel_focus(){
-	$(".media-frame-content,.media-sidebar").removeClass('show');
-	$('.overlay').removeClass('show');
-	mobile_position = false;
-}
-function close_overlay(){
-	$(".media-frame-content,.media-sidebar").removeClass('show');
-	$('.overlay').removeClass('show');
-	let message = $("#mobile_value");
-	if(mobile_position){
-		m_left = bgX;
-		m_right = bgY;
-		$('#bg_pos_mobile_id').val(bgX+'% '+bgY+'%').trigger("change");
-		if(bgX!=50 && bgY!=50){
-			$('#mobile_value').html('<b>Mobile</b>: '+bgX+'% '+bgY+'%');
-			$("#label_mobile").attr('value','Change');
-			$("#reset_mobile").show();
-		}
-		else{
-			if($("#bg_pos_desktop_id").val()=='50% 50%')
-				$('#mobile_value').html('<b>Mobile</b>: '+centered_text);
-			else
-				$('#mobile_value').html('<b>Mobile</b>: '+mobile_use_desktop_text);
-			$("#label_mobile").attr('value','Set');
+	function initValues() {
+		$hiddenInput = $("#focal_point_hidden_input");
+		$overlay = $("#focal-point-overlay");
+		$pin = $overlay.find(".pin");
+		$resetBtn = $("#focal-point-reset");
+		$updateBtn = $("#focal-point-set");
+		$valueHolder = $("#focal-point-value");
+		let _focalXY = $hiddenInput.val().replaceAll("%", "").split(" ");
+
+		focalX = _focalXY[0];
+		focalY = _focalXY[1];
+	}
+	function cancelFocus() {
+		$(".media-frame-content, .media-sidebar").removeClass("show");
+		$overlay.removeClass("show");
+	}
+	function closeOverlay() {
+		$(".media-frame-content, .media-sidebar").removeClass("show");
+		$overlay.removeClass("show");
+
+		$hiddenInput.val(`${focalX}% ${focalY}%`).trigger("change");
+
+		if (focalX != 50 && focalY != 50) {
+			$valueHolder.html(`${focalX}% ${focalY}%`);
+			$updateBtn.attr("value", WP_Image_Focal_Point.labels.change);
+			$resetBtn.removeClass("button-disabled").attr("aria-disabled", null);
+		} else {
+			$valueHolder.html(WP_Image_Focal_Point.labels.default);
+			$updateBtn.attr("value", WP_Image_Focal_Point.labels.set);
+			$resetBtn.addClass("button-disabled").attr("aria-disabled", "true");
 		}
 	}
-	else{
-		d_left = bgX;
-		d_right = bgY;
-		$('#bg_pos_desktop_id').val(bgX+'% '+bgY+'%').trigger("change");
-		if(bgX!=50 && bgY!=50){
-			$('#desktop_value').html('<b>Desktop</b>: '+bgX+'% '+bgY+'%');
-			$("#label_desktop").attr('value','Change');
-			if($("#bg_pos_mobile_id").val()=='50% 50%')
-				message.html('<b>Mobile</b>: Desktop focus point (default)');
-			$("#reset_desktop").show();
+	function setFocus() {
+		$(".media-frame-content, .media-sidebar").addClass("show");
+		$(".media-toolbar, .media-menu-item").css("z-index", 0);
+		initValues();
+		$pin.css({ left: `${focalX}%`, top: `${focalY}%` });
+		$overlay.addClass("show");
+	}
+	function resetFocus() {
+		if ($resetBtn.attr("aria-disabled") === "true") {
+			return;
 		}
-		else{
-			$("#desktop_value").html('<b>Desktop</b>: '+centered_text);
-			if($("#bg_pos_desktop_id").val()!='50% 50%')
-				message.html('<b>Mobile</b>: '+centered_text);
-			$("#label_desktop").attr('value','Set');
-		}
+		focalX = 50;
+		focalY = 50;
+		$pin.css({ left: `${focalX}%`, top: `${focalY}%` });
+		$resetBtn.addClass("button-disabled").attr("aria-disabled", "true");
+		closeOverlay();
 	}
-	mobile_position = false;
-}
-function set_focus(mobile=false,disable){
-	$(".media-frame-content,.media-sidebar").addClass('show');
-	$(".media-toolbar,.media-menu-item").css('z-index',0);
-	set_bg_values();
-	$('.overlay').addClass('show'); 
-	
-	let container = $(".image_focus_point").find('.container');
-	let image = $(".image_focus_point").find('img');
-	containerHt = container.height();
-	containerWt = container.width();
-	imageWt = image.width();
-	imageHt = image.height();
-	
-	if(mobile){
-		mobile_position = true;
-		let left = (imageWt*(m_left/100))+((containerWt-imageWt)/2);
-		let top = (imageHt*(m_right/100))+((containerHt-imageHt)/2);
-		$('.pin').css({'left':left+'px','top':top+'px'});		
-		$("#mobile_msg").html('');
-	}
-	else{	
-		let left = (imageWt*(d_left/100))+((containerWt-imageWt)/2);
-		let top = (imageHt*(d_right/100))+((containerHt-imageHt)/2);
-		$('.pin').css({'left':left+'px','top':top+'px'});	
-	}
-	if(disable)
-		$("#reset_btn").attr('disabled','');
-}
-function reset_focus(mobile=false){
-	bgX = 50;
-	bgY = 50;
-	$(".overlay").find('.pin').css({'left':'50%','top':'50%'});
-	if(mobile){
-		mobile_position = true;
-		$("#reset_mobile").hide();
-	}
-	else{
-		$("#reset_desktop").hide();
-	}
-	close_overlay();
-}
-$(document).on('click',".overlay .container img",function(e){	
-	let Offset = $(this).offset(); 
-	
-	let relX = e.pageX - Offset.left;
-	let relY = e.pageY - Offset.top;
-	bgX = Math.round((relX/imageWt)*100);
-	bgY = Math.round((relY/imageHt)*100);
-	
-	let left = (imageWt*(bgX/100))+((containerWt-imageWt)/2);
-	let top = (imageHt*(bgY/100))+((containerHt-imageHt)/2);
-	
-	$('.pin').css({'left':left+'px','top':top+'px'});	
-});
+	$(document).on("click", "#focal-point-overlay .pin-field img", function (e) {
+		const $this = $(this);
+		let offset = $this.offset();
+		let relX = e.pageX - offset.left;
+		let relY = e.pageY - offset.top;
+
+		focalX = Math.round((relX / $this.width()) * 100);
+		focalY = Math.round((relY / $this.height()) * 100);
+		$pin.css({ left: `${focalX}%`, top: `${focalY}%` });
+	});
+
+	WP_Image_Focal_Point.setFocus = setFocus;
+	WP_Image_Focal_Point.resetFocus = resetFocus;
+	WP_Image_Focal_Point.cancelFocus = cancelFocus;
+	WP_Image_Focal_Point.closeOverlay = closeOverlay;
+})(jQuery, WP_Image_Focal_Point);
